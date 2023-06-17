4581e7c6abf3a82db9aa807f8c50f3636033bdbd
Mishal <91066601+mishalhossin@users.noreply.github.com>
2023-06-17 22:45:28 +0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import 'module-alias/register';
 
 import express from 'express';
 import bodyParser from 'body-parser';
-import {ControllersLoader} from 'simple-ts-express-decorators';
-import {NsfwController} from 'app/controllers/NsfwController';
+import { ControllersLoader } from 'simple-ts-express-decorators';
+import { NsfwController } from 'app/controllers/NsfwController';
 
 const app = express();
 
@@ -14,4 +14,10 @@ new ControllersLoader({
   controllers: [NsfwController]
 }).load(app);
 
-app.listen(3000);
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.send('Use POST instead of /classify with an `image` field');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
